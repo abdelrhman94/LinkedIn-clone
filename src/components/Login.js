e53c35384b4d9db338f259da46b0fded0a74e4c9
Abdelrhman Saied <abdelrhman.s.farag@gmail.com>
2021-07-13 01:17:52 +0200

    Nav

---
+++ src/components/Login.js
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Nav>
+        <a href="/">
+          <img src="/images/login-logo.svg" alt="" />
+        </a>
+        <div>
+          <Join>Join Now</Join>
+          <SingIn>Sing in</SingIn>
+        </div>
+      </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+        </Hero>
+      </Section>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0px;
+`;
+const Nav = styled.nav`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+
+const Join = styled.a`
+  padding: 16px;
+  font-size: 10px 12px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    cursor: pointer;
+    text-decoration: none;
+  }
+`;
+
+const SingIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 16px;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+const Section =styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 768px){
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width:100%
+  h1{
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+     
+  }
+`;
+
+export default Login;
